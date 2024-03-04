345e1e59e23706a0699c9326669a4114d1609080
Darren <nardol.darren@gmail.com>
2024-03-04 23:14:51 +0100

    add meta description/readme

--- README.md
+++ README.md
@@ -1,46 +1,31 @@
 # Getting Started
 
-This app show Joe DOE's github information and is hosted on codesandbox with the URL :
+Cette application, entièrement réalisé avec React et Bootstrap, montre le profil de John Doe et est consultable à partir de cette URL :
 
 https://codesandbox.io/p/github/Dada-nol/John-Doe-React/main?import=true&layout=%257B%2522sidebarPanel%2522%253A%2522EXPLORER%2522%252C%2522rootPanelGroup%2522%253A%257B%2522direction%2522%253A%2522horizontal%2522%252C%2522contentType%2522%253A%2522UNKNOWN%2522%252C%2522type%2522%253A%2522PANEL_GROUP%2522%252C%2522id%2522%253A%2522ROOT_LAYOUT%2522%252C%2522panels%2522%253A%255B%257B%2522type%2522%253A%2522PANEL_GROUP%2522%252C%2522contentType%2522%253A%2522UNKNOWN%2522%252C%2522direction%2522%253A%2522vertical%2522%252C%2522id%2522%253A%2522clsmbn3uk00062a67pc0ig6jq%2522%252C%2522sizes%2522%253A%255B70%252C30%255D%252C%2522panels%2522%253A%255B%257B%2522type%2522%253A%2522PANEL_GROUP%2522%252C%2522contentType%2522%253A%2522EDITOR%2522%252C%2522direction%2522%253A%2522horizontal%2522%252C%2522id%2522%253A%2522EDITOR%2522%252C%2522panels%2522%253A%255B%257B%2522type%2522%253A%2522PANEL%2522%252C%2522contentType%2522%253A%2522EDITOR%2522%252C%2522id%2522%253A%2522clsmbn3uk00022a67ngmv7gql%2522%257D%255D%257D%252C%257B%2522type%2522%253A%2522PANEL_GROUP%2522%252C%2522contentType%2522%253A%2522SHELLS%2522%252C%2522direction%2522%253A%2522horizontal%2522%252C%2522id%2522%253A%2522SHELLS%2522%252C%2522panels%2522%253A%255B%257B%2522type%2522%253A%2522PANEL%2522%252C%2522contentType%2522%253A%2522SHELLS%2522%252C%2522id%2522%253A%2522clsmbn3uk00042a67kb1ga37t%2522%257D%255D%252C%2522sizes%2522%253A%255B100%255D%257D%255D%257D%252C%257B%2522type%2522%253A%2522PANEL_GROUP%2522%252C%2522contentType%2522%253A%2522DEVTOOLS%2522%252C%2522direction%2522%253A%2522vertical%2522%252C%2522id%2522%253A%2522DEVTOOLS%2522%252C%2522panels%2522%253A%255B%257B%2522type%2522%253A%2522PANEL%2522%252C%2522contentType%2522%253A%2522DEVTOOLS%2522%252C%2522id%2522%253A%2522clsmbn3uk00052a67h3uxb8pb%2522%257D%255D%252C%2522sizes%2522%253A%255B100%255D%257D%255D%252C%2522sizes%2522%253A%255B50%252C50%255D%257D%252C%2522tabbedPanels%2522%253A%257B%2522clsmbn3uk00022a67ngmv7gql%2522%253A%257B%2522tabs%2522%253A%255B%257B%2522id%2522%253A%2522clsmbn3uk00012a67wifp4jwm%2522%252C%2522mode%2522%253A%2522permanent%2522%252C%2522type%2522%253A%2522FILE%2522%252C%2522filepath%2522%253A%2522%252FREADME.md%2522%252C%2522state%2522%253A%2522IDLE%2522%257D%255D%252C%2522id%2522%253A%2522clsmbn3uk00022a67ngmv7gql%2522%252C%2522activeTabId%2522%253A%2522clsmbn3uk00012a67wifp4jwm%2522%257D%252C%2522clsmbn3uk00052a67h3uxb8pb%2522%253A%257B%2522id%2522%253A%2522clsmbn3uk00052a67h3uxb8pb%2522%252C%2522activeTabId%2522%253A%2522clsmd9u3j01ny2a67vlj9db28%2522%252C%2522tabs%2522%253A%255B%257B%2522type%2522%253A%2522ENV_SETUP%2522%252C%2522id%2522%253A%2522clsmd9u3j01ny2a67vlj9db28%2522%252C%2522mode%2522%253A%2522permanent%2522%257D%255D%257D%252C%2522clsmbn3uk00042a67kb1ga37t%2522%253A%257B%2522tabs%2522%253A%255B%257B%2522id%2522%253A%2522clsmbn3uk00032a67kkm8fw74%2522%252C%2522mode%2522%253A%2522permanent%2522%252C%2522type%2522%253A%2522TERMINAL%2522%252C%2522shellId%2522%253A%2522clsmbn577000hddfbawaeh1ns%2522%257D%255D%252C%2522id%2522%253A%2522clsmbn3uk00042a67kb1ga37t%2522%252C%2522activeTabId%2522%253A%2522clsmbn3uk00032a67kkm8fw74%2522%257D%257D%252C%2522showDevtools%2522%253Atrue%252C%2522showShells%2522%253Atrue%252C%2522showSidebar%2522%253Atrue%252C%2522sidebarPanelSize%2522%253A15%257D
 
-For more information you can visit Joe DOE website with this URL :
+Aperçu général :
 
-http://john-doe-cv.alwaysdata.net/index.html
+Hormis la page de profil, le header et le footer sont identiques sur toutes les pages.
 
-## Available Scripts
+Le footer reprend les informations de contact de John Doe, ses réseaux sociaux, les liens des différentes pages du site, ses réalisation ainsi que ses derniers articles.
 
-In the project directory, you can run:
+Le header reprend toutes les pages du site et le logo renvoit à la page d'accueil.
 
-### `npm start`
+Librairies installées :
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+React-rooter
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+L'application dispose d'un système de rootage pour l'ajout de plusieurs pages
 
-### `npm test`
+React-icons
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Le bouton scroll to top a été réalisé en utilisant la librairie react-icons.
 
-### `npm run build`
+Bootstrap
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+Une grande partie de la mise en page à été réalisé à l'aide de bootstrap.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+Les projets et les articles de blog et les blocs de "services" ont été réalisé à l'aide de la classe Card de bootstrap.
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+Le formulaire de la page de contact à l'aide de la classe Form, les boutons grâce à la classe Btn et la progress bar à l'aide de la classe progressbar
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,215 +3,217 @@ import ScrollButton from "./ScrollButton";
 
 const Footer = () => {
   return (
-    <footer className="bg-secondary-subtle">
-      <ScrollButton></ScrollButton>
-      <div className="container text-center p-3">
-        <div className="row">
-          <div className="col-lg col-md-6">
-            <h5>Jhon Doe</h5>
-            <p>40 Rue Laure Diebold</p>
-            <p>69009 Lyon, France</p>
-            <p>Téléphone : 06 20 30 40 50</p>
-            <a
-              target="_blank"
-              href="https://github.com/github-john-doe"
-              rel="noreferrer"
-            >
-              <img src="img/github.png" alt="Github" />
-            </a>
-            <a
-              target="_blank"
-              href="https://www.facebook.com/darren.nardol.7/"
-              rel="noreferrer"
-            >
-              <img src="img/facebook(1).png" alt="facebook" />
-            </a>
-            <a
-              target="_blank"
-              href="https://www.instagram.com/dada_nol/"
-              rel="noreferrer"
-            >
-              <img src="img/instagram.png" alt="instagram" />
-            </a>
-          </div>
-          <div className="col-lg col-md-6">
-            <h5>Liens utiles</h5>
-            <div>
-              <svg
-                className="me-2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
-              </svg>
-
-              <Link to="/">Home</Link>
-            </div>
-
-            <div>
-              <svg
-                className="me-2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
-              </svg>
-              <Link to="/services">Services</Link>
-            </div>
-
-            <div>
-              <svg
-                className="me-2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
-              </svg>
-
-              <Link to="/contact">Me contacter</Link>
-            </div>
-
-            <div>
-              <svg
-                className="me-2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
-              </svg>
-
-              <Link to="/mL">Mentions légales</Link>
-            </div>
-            <div>
-              <svg
-                className="me-2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
-              </svg>
-
-              <Link to="/profil">Profil Github</Link>
-            </div>
-          </div>
-          <div className="col-lg col-md-6">
-            <h5>Mes dernières réalisations</h5>
-
-            <div>
-              <svg
-                className="me-2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
-              </svg>
+    <footer>
+      <div className="bg-secondary-subtle">
+        <ScrollButton></ScrollButton>
+        <div className="container text-center p-3">
+          <div className="row">
+            <div className="col-lg col-md-6">
+              <h5>Jhon Doe</h5>
+              <p>40 Rue Laure Diebold</p>
+              <p>69009 Lyon, France</p>
+              <p>Téléphone : 06 20 30 40 50</p>
               <a
                 target="_blank"
-                href="https://github.com/Dada-nol/Premier-cahier-des-charges.git"
+                href="https://github.com/github-john-doe"
                 rel="noreferrer"
               >
-                Projet 1
+                <img src="img/github.png" alt="Github" />
               </a>
-            </div>
-
-            <div>
-              <svg
-                className="me-2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
-              </svg>
-
               <a
                 target="_blank"
-                href="https://github.com/Dada-nol/CV-Darren.git"
+                href="https://www.facebook.com/darren.nardol.7/"
                 rel="noreferrer"
               >
-                Projet 2
+                <img src="img/facebook(1).png" alt="facebook" />
               </a>
-            </div>
-
-            <div>
-              <svg
-                className="me-2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
-              </svg>
-
               <a
                 target="_blank"
-                href="https://github.com/Dada-nol/Dynamiser-un-espace-commentaire.git"
+                href="https://www.instagram.com/dada_nol/"
                 rel="noreferrer"
               >
-                Projet 3
+                <img src="img/instagram.png" alt="instagram" />
               </a>
             </div>
-          </div>
-          <div className="col-lg col-md-6">
-            <h5>Mes derniers articles</h5>
+            <div className="col-lg col-md-6">
+              <h5>Liens utiles</h5>
+              <div>
+                <svg
+                  className="me-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
+                </svg>
 
-            <div>
-              <svg
-                className="me-2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
-              </svg>
+                <Link to="/">Home</Link>
+              </div>
 
-              <a
-                target="_blank"
-                href="https://developer.mozilla.org/fr/docs/Learn/CSS/CSS_layout/Responsive_Design"
-                rel="noreferrer"
-              >
-                Article 1
-              </a>
+              <div>
+                <svg
+                  className="me-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
+                </svg>
+                <Link to="/services">Services</Link>
+              </div>
+
+              <div>
+                <svg
+                  className="me-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
+                </svg>
+
+                <Link to="/contact">Me contacter</Link>
+              </div>
+
+              <div>
+                <svg
+                  className="me-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
+                </svg>
+
+                <Link to="/mL">Mentions légales</Link>
+              </div>
+              <div>
+                <svg
+                  className="me-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
+                </svg>
+
+                <Link to="/profil">Profil Github</Link>
+              </div>
             </div>
+            <div className="col-lg col-md-6">
+              <h5>Mes dernières réalisations</h5>
 
-            <div>
-              <svg
-                className="me-2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
-              </svg>
+              <div>
+                <svg
+                  className="me-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
+                </svg>
+                <a
+                  target="_blank"
+                  href="https://github.com/Dada-nol/Premier-cahier-des-charges.git"
+                  rel="noreferrer"
+                >
+                  Projet 1
+                </a>
+              </div>
 
-              <a
-                target="_blank"
-                href="https://developer.mozilla.org/fr/docs/Glossary/SEO"
-                rel="noreferrer"
-              >
-                Article 2
-              </a>
+              <div>
+                <svg
+                  className="me-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
+                </svg>
+
+                <a
+                  target="_blank"
+                  href="https://github.com/Dada-nol/CV-Darren.git"
+                  rel="noreferrer"
+                >
+                  Projet 2
+                </a>
+              </div>
+
+              <div>
+                <svg
+                  className="me-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
+                </svg>
+
+                <a
+                  target="_blank"
+                  href="https://github.com/Dada-nol/Dynamiser-un-espace-commentaire.git"
+                  rel="noreferrer"
+                >
+                  Projet 3
+                </a>
+              </div>
             </div>
+            <div className="col-lg col-md-6">
+              <h5>Mes derniers articles</h5>
 
-            <div>
-              <svg
-                className="me-2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
-              </svg>
+              <div>
+                <svg
+                  className="me-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
+                </svg>
 
-              <a
-                target="_blank"
-                href="https://developer.mozilla.org/fr/docs/Learn"
-                rel="noreferrer"
-              >
-                Article 2
-              </a>
+                <a
+                  target="_blank"
+                  href="https://developer.mozilla.org/fr/docs/Learn/CSS/CSS_layout/Responsive_Design"
+                  rel="noreferrer"
+                >
+                  Article 1
+                </a>
+              </div>
+
+              <div>
+                <svg
+                  className="me-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
+                </svg>
+
+                <a
+                  target="_blank"
+                  href="https://developer.mozilla.org/fr/docs/Glossary/SEO"
+                  rel="noreferrer"
+                >
+                  Article 2
+                </a>
+              </div>
+
+              <div>
+                <svg
+                  className="me-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.2 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.2 0l192 192z" />
+                </svg>
+
+                <a
+                  target="_blank"
+                  href="https://developer.mozilla.org/fr/docs/Learn"
+                  rel="noreferrer"
+                >
+                  Article 2
+                </a>
+              </div>
             </div>
           </div>
         </div>
+        <p className="text-center bg-secondary text-light">
+          Designed by Jhon Doe
+        </p>
       </div>
-      <p className="text-center bg-secondary text-light">
-        Designed by Jhon Doe
-      </p>
     </footer>
   );
 };
